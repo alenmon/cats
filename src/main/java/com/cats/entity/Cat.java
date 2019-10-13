6e19918180ab1165e3a7d9b5fde011894371e18f
alenmon <vesna.0596@gmail.com>
2019-10-13 12:49:40 +0300

    Add cat class

---
+++ src/main/java/com/cats/entity/Cat.java
@@ -0,0 +1,11 @@
+package com.cats.entity;
+
+import static java.lang.System.out;
+
+public class Cat {
+    public String color;
+
+    public static void meow() {
+        out.println("meow");
+    }
+}
