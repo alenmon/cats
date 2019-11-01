0d6b73ebe0bfc009ef374e18d59f480ab60419ca
alenmon <vesna.0596@gmail.com>
2019-11-01 23:09:25 +0300

    Add class Husky Add class Husky

--- src/main/java/com/cats/entity/Husky.java
+++ src/main/java/com/cats/entity/Husky.java
@@ -1,7 +1,21 @@
 package com.cats.entity;
 
+
+import org.json.simple.JSONObject;
+
 public class Husky extends Dog {
     public String dogBark() {
         return "Husky barks";
     }
+
+    public static void main(String args[]) {
+
+        JSONObject Husky = new JSONObject();
+        Husky.put("Lucy", "White color");
+
+        System.out.println("Name:" + Husky.get("Lucy"));
+        System.out.println("Color:" + Husky.get("White color"));
+
+    }
+
 }
